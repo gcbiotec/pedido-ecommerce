1f4444d73abc44360cd528392c3aeb04043daf5e
ronaldo <ronaldofilho2@hotmail.com>
2020-12-03 19:56:17 -0300

    Add Javadoc for repository and service

--- src/main/java/com/fundatec/pedido/repository/PedidoRepository.java
+++ src/main/java/com/fundatec/pedido/repository/PedidoRepository.java
@@ -4,6 +4,9 @@ import com.fundatec.pedido.domain.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Classe responsável pela interface com o banco de dados
+ */
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
--- src/main/java/com/fundatec/pedido/service/PedidoService.java
+++ src/main/java/com/fundatec/pedido/service/PedidoService.java
@@ -7,20 +7,37 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Classe que orquestra as requisições de pedido
+ */
 @Service
 public class PedidoService {
 
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    /**
+     * Método para encontrar todos os pedidos do repositório
+     * @return
+     */
     public List<Pedido> findAll() {
         return pedidoRepository.findAll();
     }
 
+    /**
+     * Método para salvar um pedido no repositório
+     * @param pedido
+     * @return
+     */
     public Pedido save(Pedido pedido) {
         return pedidoRepository.saveAndFlush(pedido);
     }
 
+    /**
+     * Método para procurar um pedido por um identificador
+     * @param id
+     * @return
+     */
     public Pedido findById(Long id) {
         return pedidoRepository.findById(id).get();
     }
